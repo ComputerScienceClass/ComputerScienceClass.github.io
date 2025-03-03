edf24272ea022768f87edadd3f7f5a8c4dee52c5
ComputerScienceClass <1802250@spsd.sk.ca>
2025-03-03 13:38:12 -0600

    Started assignment

--- Index.md
+++ Index.md
@@ -2,7 +2,7 @@
 - [Color Swap Timer](colour-times-swap)
 - [traffic-light](traffic-light)
 # CS30 Projects
-- [Interactive Scene[interactive-scene]]
+- [interactive-scene](interactive-scene)
 
 first opne in visual studio
 then press shift crtl and p and opne p5.js
--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,1 +1,49 @@
-
+function setup() {
+    createCanvas(500, 500);
+    
+    let button = createButton("Click me");
+    button.position(220, 250);
+    button.mousePressed(repaint);
+    
+    
+    let thisButton = createButton("Get ball");
+    thisButton.position(50, 400);
+    thisButton.mousePressed(ball);
+    
+    let thisTriangle = createButton("Get triangle");
+    thisTriangle.position(150, 400);
+    thisTriangle.mousePressed(triangleShape);
+    
+    let thisRect = createButton("Get rectangle");
+    thisRect.position(275, 400);
+    thisRect.mousePressed(rectangleShape);
+  }
+  
+  function draw() {
+    //background(0, 100, 200);
+    //circle(250, 250, 80)
+    //noStroke();
+  }
+  
+  
+  function repaint() {
+    let g = random(["magenta", "lightgreen", "lightyellow"]);
+    background(g);
+  }
+  
+  function ball() {
+    let ballMore = circle(50, 50, 50);
+    let d = random(ballMore);
+  }
+  
+  
+  function triangleShape() {
+    let triangleMore = triangle(-20, 25, 8, -30, 36, 25);
+    let e = random(triangleMore);
+  }
+  
+  function rectangleShape() {
+    let rectangleMore = rect(230, 30, 55, 40);
+    let r = random(rectangleMore);
+    
+  }
