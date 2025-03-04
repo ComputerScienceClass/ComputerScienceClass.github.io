8241dd23be0356b42e621baa8fb8fd0ee61acd66
ComputerScienceClass <1802250@spsd.sk.ca>
2025-03-04 13:14:18 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,49 +1,66 @@
+// Interactive Scene
+// Carolena
+// March 3, 2025
+
+
+let backgroundColour;
+let shape;
+
+
 function setup() {
-    createCanvas(10000, 10000);
-    
-    let button = createButton("Click me");
-    button.position(750, 250);
-    button.mousePressed(repaint);
-    
-    
-    let thisButton = createButton("Get ball");
-    thisButton.position(150, 400);
-    thisButton.mousePressed(ball);
-    
-    let thisTriangle = createButton("Get triangle");
-    thisTriangle.position(250, 400);
-    thisTriangle.mousePressed(triangleShape);
-    
-    let thisRect = createButton("Get rectangle");
-    thisRect.position(375, 400);
-    thisRect.mousePressed(rectangleShape);
+  createCanvas(windowWidth, windowHeight);
+  backgroundColour = color(220);
+  textAlign(CENTER, CENTER);
+  textSize(14);
+}
+
+
+function draw() {
+  background(backgroundColour);
+
+
+  fill(200);
+  rect(10, 10, 80, 40);
+  rect(100, 10, 80, 40);
+  rect(190, 10, 100, 40);
+
+
+  fill(0)
+  text("Get square", 50, 30);
+  text("Get triangle", 140, 30);
+  text("Get rectangle", 240, 30);
+
+
+  fill(150, 0, 150);
+  noStroke();
+  let x = width / 2;
+  let y = height / 2;
+
+
+  if (shape === "square") {
+    rect(x - 50, y - 50, 100, 100);
+  } else if (shape === "triangle") {
+    triangle(x, y - 60, x - 60, y + 40, x + 60, y + 40);
+  } else if (shape === "rectangle") {
+    rect(x - 75, y - 40, 150, 80);
   }
-  
-  function draw() {
-    //background(0, 100, 200);
-    //circle(250, 250, 80)
-    //noStroke();
+
+
+}
+
+
+function mouseClicked() {
+  if (mouseX > 10 && mouseX < 90 && mouseY > 10 && mouseY < 50) {
+    shape = "square";
+  } else if (mouseX > 100 && mouseX < 180 && mouseY > 10 && mouseY < 50) {
+    shape = "triangle";
+  } else if (mouseX > 190 && mouseX < 270 && mouseY > 10 && mouseY < 50) {
+    shape = "rectangle";
+  } else {
+    backgroundColour = color(random(255), random(255), random(255))
   }
-  
-  
-  function repaint() {
-    let g = random(["magenta", "lightgreen", "lightyellow"]);
-    background(g);
-  }
-  
-  function ball() {
-    let ballMore = circle(50, 50, 50);
-    let d = random(ballMore);
-  }
-  
-  
-  function triangleShape() {
-    let triangleMore = triangle(-20, 25, 8, -30, 36, 25);
-    let e = random(triangleMore);
-  }
-  
-  function rectangleShape() {
-    let rectangleMore = rect(230, 30, 55, 40);
-    let r = random(rectangleMore);
-    
-  }
+}
+
+
+
+
