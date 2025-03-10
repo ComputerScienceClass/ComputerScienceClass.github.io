d5336f3f66740658828c37c1d9a32bbff4a5350b
ComputerScienceClass <1802250@spsd.sk.ca>
2025-03-10 13:33:50 -0600

    Update sketch.js

--- bouncing-ball/sketch.js
+++ bouncing-ball/sketch.js
@@ -6,35 +6,57 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// Bouncing Ball Object Demo
+
+let ballArray = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spawnBall();
 }
 
 function draw() {
   background(220);
+
   for (let ball of ballArray) {
-    ball.X += ball.dx;
-    ball.Y += ball.dy;
-    
+    //move ball
+    ball.x += ball.dx;
+
+    ball.y += ball.dy;
+
+    //teleport around edge of screen
     if (ball.x > width) {
-      ball.x += width;
+      ball.x -= width;
     }
+    else if (ball.x < 0) {
+      ball.x = width;
+    }
+    if (ball.y > height) {
+      ball.y = -ball.radius < 0) {
+
+      }
+    
     else if (ball.y < 0) {
-      ball.y ++ height;
+      ball.y += height;
     }
-    
-    
+
+    //display ball
     fill("red");
     circle(ball.x, ball.y, ball.radius * 2);
   }
+}
+
+function mousePressed() {
+  spawnBall();
 }
 
 function spawnBall() {
   let someBall = {
     x: random(width),
     y: random(height),
-    radius: random(15, 60),
+    radius: random(100, 200),
     dx: random(-5, 5),
     dy: random(-5, 5),
-  }
+  };
+  ballArray.push(someBall);
 }
